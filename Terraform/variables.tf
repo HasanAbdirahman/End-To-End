4ff1ace3faa1bbeb0a984a52b41a7d2b26970217
HasanAbdirahman <108181341+HasanAbdirahman@users.noreply.github.com>
2025-08-07 16:40:44 +0000

    fixed minor issuee

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,9 +1,9 @@
 variable subnets_counts {
-    type: number
-    default: 2
+    type =  number
+    default =  2
 }
 
 variable cidr_block { 
-    type: list(string)
-    default: ["10.0.1.0/24", "10.0.2.0/24"]
+    type =  list(string)
+    default =  ["10.0.1.0/24", "10.0.2.0/24"]
 }
