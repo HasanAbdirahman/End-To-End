b839402e7648907c0d51dce1235a694313adc058
HasanAbdirahman <108181341+HasanAbdirahman@users.noreply.github.com>
2025-08-07 17:17:59 +0000

    deleted some output file

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -2,14 +2,6 @@ output "public_subnet_ids" {
   value = aws_subnet.Terra-Public-Subnets[*].id
 }
 
-output "kubeconfig_command" {
-  value = module.eks.kubeconfig_filename
-}
-
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
 output "cluster_name" {
   value = module.eks.name
 }
